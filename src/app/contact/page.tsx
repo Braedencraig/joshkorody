06bc1a94a44f505d8291e61deedeefc1e9d2a78e
Braeden Craig <braeden.craig@gmail.com>
2023-09-07 22:01:44 -0400

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,41 +3,41 @@ import { useState } from "react";
 import Navbar from "../../components/Navbar";
 
 export default function Contact() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [formStatus, setFormStatus] = useState(null);
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   email: "",
+  //   message: "",
+  // });
+  // const [formStatus, setFormStatus] = useState(null);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({ ...prevState, [name]: value }));
-  };
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prevState) => ({ ...prevState, [name]: value }));
+  // };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("/api/sendMail", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await fetch("/api/sendMail", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(formData),
+  //     });
 
-      if (response.ok) {
-        setFormStatus("Success! Thank you for reaching out.");
-        setFormData({
-          name: "",
-          email: "",
-          message: "",
-        });
-      } else {
-        throw new Error("There was an issue submitting the form.");
-      }
-    } catch (error) {
-      setFormStatus(`Error: ${error.message}`);
-    }
-  };
+  //     if (response.ok) {
+  //       setFormStatus("Success! Thank you for reaching out.");
+  //       setFormData({
+  //         name: "",
+  //         email: "",
+  //         message: "",
+  //       });
+  //     } else {
+  //       throw new Error("There was an issue submitting the form.");
+  //     }
+  //   } catch (error) {
+  //     setFormStatus(`Error: ${error.message}`);
+  //   }
+  // };
 
   return (
     <div className="bg-white text-black h-full">
@@ -46,7 +46,13 @@ export default function Contact() {
         <h1 className="text-4xl md:text-6xl text-center font-semibold mb-10">
           CONTACT
         </h1>
-        <form
+        <a
+          className=" text-2xl text-center m-auto flex justify-center"
+          href="mailto:joshkorody@gmail.com"
+        >
+          joshkorody@gmail.com
+        </a>
+        {/* <form
           onSubmit={handleSubmit}
           className="space-y-6 max-w-[600px] m-auto"
         >
@@ -84,7 +90,7 @@ export default function Contact() {
             Submit
           </button>
         </form>
-        {formStatus && <p className="mt-6 text-center">{formStatus}</p>}
+        {formStatus && <p className="mt-6 text-center">{formStatus}</p>} */}
       </main>
     </div>
   );
